ba1452959a59677ab1f83f5c405cf45bb3f1e9b8
SLajuwomi <stephenlajuwomi@gmail.com>
2024-08-01 22:20:33 -0500

    added code to move the rectangle automatically

--- PlatformerTutorial/src/main/Game.java
+++ PlatformerTutorial/src/main/Game.java
@@ -1,14 +1,39 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
   // Create a GameWindow object
   private GameWindow gameWindow;
   // Create GamePanel object
   private GamePanel gamePanel;
+  private Thread gameThread;
+  private final int FPS_SET = 120;
 
   public Game() {
     gamePanel = new GamePanel();
     gameWindow = new GameWindow(gamePanel);
     gamePanel.requestFocus();
+    startGameLoop();
+  }
+
+  private void startGameLoop() {
+    gameThread = new Thread(this);
+    gameThread.start();
+  }
+
+  @Override
+  public void run() {
+    // how long each frame lasts in nanoseconds
+    double timePerFrame = 1000000000.0 / FPS_SET;
+    long lastFrame = System.nanoTime();
+    long now = System.nanoTime();
+
+    now = System.nanoTime();
+    while (true) {
+
+      if (now - lastFrame >= timePerFrame) {
+        gamePanel.repaint();
+        lastFrame = now;
+      }
+    }
   }
 }
--- PlatformerTutorial/src/main/GamePanel.java
+++ PlatformerTutorial/src/main/GamePanel.java
@@ -1,8 +1,10 @@
 package main;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.util.Random;
 
 import javax.swing.JPanel;
 
@@ -11,10 +13,16 @@ import inputs.MouseInputs;
 
 public class GamePanel extends JPanel {
   private MouseInputs mouseInputs;
-  private int xDelta = 100, yDelta = 100;
+  private float xDelta = 100, yDelta = 100;
+  private float xDir = 0.003f, yDir = 0.003f;
+  private int frames = 0;
+  private long lastCheck = 0;
+  private Color color = new Color(150, 255, 150);
+  private Random random;
 
   // JPanel is the picture in the frame
   public GamePanel() {
+    random = new Random();
     mouseInputs = new MouseInputs(this);
     addKeyListener(new KeyboardInputs(this));
     addMouseListener(mouseInputs);
@@ -26,18 +34,18 @@ public class GamePanel extends JPanel {
   // repaint();
   public void changeXDelta(int value) {
     this.xDelta += value;
-    repaint();
+
   }
 
   public void changeYDelta(int value) {
     this.yDelta += value;
-    repaint();
+
   }
 
   public void setRectPos(int x, int y) {
     this.xDelta = x;
     this.yDelta = y;
-    repaint();
+
   }
 
   // Need a paintCompenent so JPanel can find it
@@ -46,7 +54,43 @@ public class GamePanel extends JPanel {
     // call superclass of JPanel through paintComponent
     super.paintComponent(g);
 
+    updateRectangle();
+
+    g.setColor(color);
     // (X, Y, WIDTH, HEIGHT)
-    g.fillRect(xDelta, yDelta, 200, 50);
+    g.fillRect((int) xDelta, (int) yDelta, 200, 50);
+
+    frames++;
+    // If one second have passed since the last fps check
+    // we do a new fps check
+    // Save the new FPS check ast the lastFps and repeat
+    if (System.currentTimeMillis() - lastCheck >= 1000) {
+      lastCheck = System.currentTimeMillis();
+      System.out.println("FPS: " + frames);
+      frames = 0;
+    }
+
+  }
+
+  private void updateRectangle() {
+    xDelta += xDir;
+    if (xDelta > 400 || xDelta < 0) {
+      xDir *= -1;
+      color = getRndColor();
+    }
+
+    yDelta += yDir;
+    if (yDelta > 400 || yDelta < 0) {
+      yDir *= -1;
+      color = getRndColor();
+    }
+  }
+
+  private Color getRndColor() {
+    int r = random.nextInt(255);
+    int g = random.nextInt(255);
+    int b = random.nextInt(255);
+
+    return new Color(r, g, b);
   }
 }
